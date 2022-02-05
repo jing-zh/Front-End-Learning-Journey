6cc8244497516bb4991759214da26e066999f539
波罗罗 <boluoluo@boluoluodeMacBook-Air.local>
2022-02-05 15:44:37 +0800

    create tea station project

---
+++ Basics/html_css/tea-station-project/app.js
@@ -0,0 +1,24 @@
+// setup nav
+const navBtn = document.getElementById('nav-btn');
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
+// show nav
+if (navBtn) {
+  navBtn.addEventListener('click', () => {
+    navbar.classList.add('showNav');
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navbar.classList.remove('showNav');
+  });
+}
+// close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
+//setup date
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
