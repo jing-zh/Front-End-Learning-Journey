8d167f1fcf8f6e7a19b629813fc0d565317f1222
波罗罗 <Zhou.Jing21@outlook.com>
2022-05-20 18:58:53 +0800

    update

--- vue-practice/app.js
+++ vue-practice/app.js
@@ -1,15 +1,44 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "http://google.com",
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandson Sanderson",
-      age: 45,
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e) {
+      console.log(e);
+    },
+    handleMousemove(e) {
+      (this.x = e.offsetX), (this.y = e.offsetY);
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
